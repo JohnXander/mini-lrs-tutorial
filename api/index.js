a6994f485c7d7db696f2f42924bc7b8a70bf26ce
John Bloxam <johnblox5@gmail.com>
2024-04-04 15:58:54 +0100

    connect to mongo

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_PATH)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
